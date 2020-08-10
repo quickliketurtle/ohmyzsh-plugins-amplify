785ca6c37c680f453bdf7c7f758eead4e5e70bd2
Jeff Finley <jeff.finley@fivetalent.com>
2020-08-09 18:31:46 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Amplify Plugin
+
+This plugin adds useful aliases.
+To use it, add amplify to the plugins array in your `.zshrc` file:
+
+```bash
+plugins=(... amplify)
+```
---
+++ amplify.plugin.zsh
@@ -0,0 +1,51 @@
+# Aliases
+
+## Base
+alias a='amplify'
+
+## Init
+alias ai='amplify init'
+
+## Configure
+alias ac='amplify configure'
+
+## Push
+alias ap='amplify push'
+alias apy='amplify push -y'
+
+## Pull
+alias al='amplify pull'
+alias aly='amplify pull -y'
+
+## Publish
+alias apub='amplify publish'
+
+## Serve
+alias as='amplify serve'
+
+## Status
+alias ast='amplify status'
+
+## Delete
+alias adel='amplify delete'
+
+## Categories
+### Not sure these are needed...
+
+## Mock
+alias am='amplify mock'
+
+## Codegen
+alias acg='amplfiy codegen'
+
+## Env
+alias ae='amplify env'
+alias aea='amplify env add'
+alias aep='amplify env pull'
+alias aec='amplify env checkout'
+alias aeg='amplify env get'
+alias aei='amplify env import'
+alias aer='amplify env remove'
+
+## Console
+alias ac='amplify console'
